63869c18a79bf58efd2595fc81010acd491101c7
Neha Kherde <u1205893@utah.edu>
2018-10-31 22:34:24 -0600

    Basic changes in world html and js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,33 @@
-let treeMap = new treeMap();
+loadData().then(data => {
+
+    const worldMap = new Map(data, updateCountry);
+
+    d3.json('data/world.json').then(mapData => {
+        worldMap.drawMap(mapData);
+    });
+});
+
+// ******* DATA LOADING *******
+async function loadFile(file) {
+    let data = await d3.csv(file).then(d => {
+        let mapped = d.map(g => {
+            for (let key in g) {
+                let numKey = +key;
+                if (numKey) {
+                    g[key] = +g[key];
+                }
+            }
+            return g;
+        });
+        return mapped;
+    });
+    return data;
+}
+
+async function loadData() {
+    let cmu = await loadFile('../../data/child-mortality.csv');
+
+    return {
+        'child-mortality': cmu,
+    };
+}
