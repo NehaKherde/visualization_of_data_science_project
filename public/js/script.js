24e22423dea2e06670529d08c45985b930f5c0a2
Harshitha <harshitha.manduva@gmail.com>
2018-11-07 23:17:54 -0700

    added new branch

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,2 @@
 let treeMap = new treeMap();
+treeMap.update();
--- public/js/treeMap.js
+++ public/js/treeMap.js
@@ -1,8 +1,13 @@
 class treeMap {
     constructor(){
         console.log("treeMap");
+        causesOfDeathData = [];
     };
     update(){
-        
+        let that = this;
+        d3.csv("../data/annual-number-of-deaths-by-cause.csv").then(causesOfDeath => {
+            that.causesOfDeathData.push(causesOfDeath);
+        });
+        console.log(causesOfDeathData); 
     };
 }
